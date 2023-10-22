84e3a5d2e7e1df5ad692abaa240f00207eaad9f0
efat56 <104213039+efat56@users.noreply.github.com>
2023-10-22 14:44:10 +0530

    day3Leve1Done

---
+++ AssignmentDay3/Program.cs
@@ -0,0 +1,42 @@
+﻿
+
+// Define the delegate.....
+delegate int MathNaticalOperation(int num);
+class Program
+{
+    static int Square(int x)
+    {
+        return x * x;
+    }
+
+
+    static int Cube(int x)
+    {
+        return x * x * x;
+    }
+
+    static void Main(string[] args)
+    {
+        // Assign methods to the delegate
+        MathNaticalOperation square = Square;
+
+        MathNaticalOperation cube = Cube;
+
+        Console.Write("Enter an Integer.....");
+        if(int.TryParse(Console.ReadLine(),out int num))
+        {
+            int squareResult = Square(num);
+            int cubeResult = Cube(num);
+
+            Console.WriteLine($"Square of {num} is {squareResult} ");
+            Console.WriteLine($"Cube of {num} is {cubeResult} ");
+        }
+        else
+        {
+            Console.WriteLine("Invalid input. Please enter a valid integer.");
+        }
+
+    }
+
+
+}
---
+++ AssignmentDay3Level_1/Program.cs
@@ -0,0 +1,38 @@
+﻿using static AssignmentDay3Level_1.Program;
+
+namespace AssignmentDay3Level_1
+{
+    internal class Program
+    {
+        //delegate declaration....
+        public delegate void voidDelegate();
+        static void Main(string[] args)
+        {
+            voidDelegate del1 = print1;
+            //print1();
+            MyClass.ExecuteDelegate(del1);
+           
+        }
+
+        private static void print1()
+        {
+            Console.WriteLine("This is a print1 method......");
+        }
+    }
+
+    class MyClass
+    {
+
+        public static void ExecuteDelegate(voidDelegate del)
+        {
+
+            del();
+        }
+
+    }
+
+
+
+
+
+}
